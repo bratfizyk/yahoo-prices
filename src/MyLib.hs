14b4e8bd97f412565e2cb23af74e0ab2ab3803e1
Alojzy Leszcz <>
2020-12-31 02:22:56 +0100

    Adding Cassava types.

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,20 +1,56 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module MyLib (someFunc) where
-
+    
 import Control.Lens ((^.))
-import Data.ByteString.Lazy.Char8 (unpack)
+import Data.ByteString.Char8 (unpack)
+import Data.Csv (FromField(..), FromNamedRecord(..), (.:), decodeByName)
+import Data.Time (defaultTimeLocale)
+import Data.Time.Calendar (Day)
+import Data.Time.Format (parseTimeM)
 import Network.Wreq (get, responseBody)
+
+import qualified Data.Vector as V
+
+data Price = Price {
+    date     :: Day,
+    open     :: Double,
+    high     :: Double,
+    low      :: Double,
+    close    :: Double,
+    adjClose :: Double,
+    volume   :: Double
+} deriving (Show, Eq)
+
+instance FromField Day where
+    parseField = parseTimeM True defaultTimeLocale "%Y-%m-%d" . unpack
+
+instance FromNamedRecord Price where
+    parseNamedRecord r = 
+        Price 
+            <$> r .: "Date" 
+            <*> r .: "Open"
+            <*> r .: "High"
+            <*> r .: "Low"
+            <*> r .: "Close"
+            <*> r .: "Adj Close"
+            <*> r .: "Volume"
 
 url :: String
 url = "http://query1.finance.yahoo.com/v7/finance/download/"
 
 ticker :: String
-ticker = "OGZPY"
+ticker = "C38U.SI"
+
+processPrice :: Price -> IO ()
+processPrice = putStrLn . show
 
 someFunc :: IO ()
 someFunc = do
     r <- get (url ++ ticker)
     let body = r ^. responseBody
-    putStrLn (unpack body)
+    case decodeByName body of
+        Left err -> putStrLn err
+        Right (_, v) ->
+            V.forM_ v $ processPrice
     return ()
