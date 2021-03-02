1e168898a11b980ad2e7475c555e1ce07eb70d02
Alojzy Leszcz <>
2021-03-02 01:50:20 +0100

    Updating shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,8 @@
 { nixpkgs ? import ./pkgs.nix {} }:
 let
   inherit (nixpkgs) pkgs;
-  inherit (pkgs) haskellPackages;
-
-  haskellDeps = ps: with ps; [
-    base bytestring cassava hspec lens time vector wreq
-  ];
-
-  ghc = pkgs.haskell.packages.ghc884.ghcWithPackages haskellDeps;
-
-  nixPackages = [
-    ghc
-  ];
 in
 pkgs.stdenv.mkDerivation {
-  name = "snadbox-haskell-workspace";
-  buildInputs = nixPackages;
+  name = "yahoo-prices-shell";
+  buildInputs = with pkgs; [ (import ./release.nix) ];
 }
