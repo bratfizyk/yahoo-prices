baafbfe5aa03e855c70a8774f718c1556fa54279
Alojzy Leszcz <>
2021-03-02 01:43:37 +0100

    Updating default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 { mkDerivation, base, bytestring, Cabal, cassava, hspec, lens, stdenv, time, vector, wreq }:
 mkDerivation {
   pname = "yahoo-price";
-  version = "0.1.0.0";
+  version = "0.1.0.2";
   src = ./.;
   isLibrary = true;
-  isExecutable = true;
+  isExecutable = false;
   executableHaskellDepends = [
     base bytestring Cabal cassava lens hspec time vector wreq
   ];
