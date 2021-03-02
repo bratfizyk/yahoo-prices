af5c8091057b80b34984e1483690d7a887d00208
Alojzy Leszcz <>
2021-03-02 01:59:58 +0100

    Simplifying shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,5 @@
-{ nixpkgs ? import ./pkgs.nix {} }:
-let
-  inherit (nixpkgs) pkgs;
-in
+{ pkgs ? import ./pkgs.nix {} }:
+
 pkgs.stdenv.mkDerivation {
   name = "yahoo-prices-shell";
   buildInputs = with pkgs; [ (import ./release.nix) ];
